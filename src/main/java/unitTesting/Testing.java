38d24e05e46a0de0bf69448b694d2ec10ed25cb6
vijaylondhe <vijay.londhe815@gmail.com>
2022-01-17 11:30:14 +0530

    Initial commit

---
+++ src/main/java/unitTesting/Testing.java
@@ -0,0 +1,14 @@
+package unitTesting;
+
+public class Testing {
+
+//	public Testing() {
+//		// TODO Auto-generated constructor stub
+//	}
+
+	public static void Display(String param ) {
+		System.out.println("Your parameter is: " + param);
+	}
+	
+	
+}
